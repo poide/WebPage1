c3a964314bb2deb6afaec74b82daa1fc33a75e9c
Pepe <pepeguis9@gmail.com>
2021-09-03 17:52:47 +0200

    Adding the code

---
+++ form.php
@@ -0,0 +1,7 @@
+<?php 
+echo "<p> Nombre: " . $_POST["Cnombre"] . "</p>";
+echo "<p> Asunto: " . $_POST["Casunto"] . "</p>";
+echo "<p> Telefono: " . $_POST["Ctlfn"] . "</p>";
+echo "<p> Email: " . $_POST["Cemail"] . "</p>";
+echo "<p> Mensaje: " . $_POST["Cmensaje"] . "</p>";
+?>
